3c452c043e75d0d085f284e58c4e1e6ebeffd441
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-08-09 11:43:03 -0300

    Insert - Inserindo registros

---
+++ model/Manager.class.php
@@ -0,0 +1,17 @@
+<?php  
+
+class Manager extends Conexao {
+
+	public function insertClient($table, $data) {
+		$pdo = parent::get_instance();
+		$fields = implode(", ", array_keys($data));
+		$values = ":".implode(", :", array_keys($data));
+		$sql = "INSERT INTO $table ($fields) VALUES ($values)";
+		$statement = $pdo->prepare($sql);
+		foreach($data as $key => $value) {
+			$statement->bindValue(":$key", $value, PDO::PARAM_STR);
+		}
+		$statement->execute();
+	}
+
+}
--- model/conexao.class.php
+++ model/conexao.class.php
@@ -2,19 +2,21 @@
 
 class Conexao
 {
+
     public static $instance;
 
     public static function get_instance()
     {
         if (!isset(self::$instance)) {
             self::$instance = new PDO(
-                "mysql:host=localhost;dbname=agendamento;",
+                "mysql:host=localhost;dbname=agendamentos;",
                 "root",
                 "",
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
             );
-
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+
+        return self::$instance;
     }
 }

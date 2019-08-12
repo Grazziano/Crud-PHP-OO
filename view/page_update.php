1eb33856d4246a4733258303eee02278cfe7e56a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-08-11 22:14:19 -0300

    Update - Parte 1

--- view/page_update.php
+++ view/page_update.php
@@ -1,5 +1,10 @@
 <?php
+include_once '../model/Conexao.class.php';
+include_once '../model/Manager.class.php';
 include_once 'dependencias.php';
+
+$manager = new Manager();
+$id = $_POST['id'];
 ?>
 
 <h2 class="text-center">
@@ -12,37 +17,48 @@ include_once 'dependencias.php';
     <div class="container">
         <div class="form-row">
 
-            <div class="col-md-6">
-                Nome: <i class="fa fa-user"></i>
-                <input class="form-control" type="text" name="name" required autofocus><br>
-            </div>
+            <?php
+            foreach ($manager->getInfo("registros", $id) as $client_info) :
+                ?>
 
-            <div class="col-md-6">
-                E-mail: <i class="fa fa-envelope"></i>
-                <input class="form-control" type="text" name="email" required><br>
-            </div>
+                <div class="col-md-6">
+                    Nome: <i class="fa fa-user"></i>
+                    <input class="form-control" type="text" name="name" value="<?= $client_info['name'] ?>" required autofocus><br>
+                </div>
 
-            <div class="col-md-4">
-                CPF: <i class="fa fa-address-card"></i>
-                <input class="form-control" type="text" name="cpf" id="cpf" required><br>
-            </div>
+                <div class="col-md-6">
+                    E-mail: <i class="fa fa-envelope"></i>
+                    <input class="form-control" type="text" name="email" value="<?= $client_info['email'] ?>" required><br>
+                </div>
 
-            <div class="col-md-4">
-                Dt. de Nascimento: <i class="fa fa-calendar"></i>
-                <input class="form-control" type="date" name="birth" required><br>
-            </div>
+                <div class="col-md-4">
+                    CPF: <i class="fa fa-address-card"></i>
+                    <input class="form-control" type="text" name="cpf" id="cpf" value="<?= $client_info['cpf'] ?>" required><br>
+                </div>
 
-            <div class="col-md-4">
-                Telefone: <i class="fab fa-whatsapp"></i>
-                <input class="form-control" type="text" name="phone" id="phone" required><br>
-            </div>
+                <div class="col-md-4">
+                    Dt. de Nascimento: <i class="fa fa-calendar"></i>
+                    <input class="form-control" type="date" name="birth" value="<?= $client_info['birth'] ?>" required><br>
+                </div>
 
-            <div class="col-md-12">
-                Endereço: <i class="fa fa-map"></i>
-                <input class="form-control" type="text" name="address" required><br>
-            </div>
+                <div class="col-md-4">
+                    Telefone: <i class="fab fa-whatsapp"></i>
+                    <input class="form-control" type="text" name="phone" id="phone" value="<?= $client_info['phone'] ?>" required><br>
+                </div>
 
-            <div class="col-md-4">
+                <div class="col-md-12">
+                    Endereço: <i class="fa fa-map"></i>
+                    <input class="form-control" type="text" name="address" value="<?= $client_info['address'] ?>" required><br>
+                </div>
+
+                <div class="col-md-4">
+
+                    <input type="hidden" name="id" value="<?= $client_info['id'] ?>">
+
+                <?php
+                endforeach;
+                ?>
+
                 <button class="btn btn-warning btn-lg">Update Client <i class="fa fa-user-edit"></i></button><br><br>
 
                 <a href="../index.php">Voltar</a>
